030d7aa122e6aaa1cba68eda97d0d62ef7f0f980
Sagar Amrutkar <scamrutkar@gmail.com>
2019-08-30 22:02:58 +0530

    added implementation of spring-websocket

---
+++ spring-websocket/src/main/java/com/javacase/sagar/Greeting.java
@@ -0,0 +1,23 @@
+package com.javacase.sagar;
+
+public class Greeting {
+	
+	private String content;
+	
+	public Greeting() {}
+
+	public Greeting(String content) {
+        this.content = content;
+    }
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+
+}
