243993c881c59d1e0550b1b9687411778e1872d5
Sagar Amrutkar <scamrutkar@gmail.com>
2020-01-22 22:26:14 +0530

    adding spring bean callback funtions implementation

--- springxmlconfigurations/src/main/java/com/javacase/sagar/Triangle.java
+++ springxmlconfigurations/src/main/java/com/javacase/sagar/Triangle.java
@@ -1,25 +1,58 @@
 package com.javacase.sagar;
 
-public class Triangle {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Triangle implements InitializingBean,DisposableBean{
 	
-	private String type;
-	private String height;
+	private Point point1;
+	private Point point2;
+	private Point point3;
 	
-	public Triangle(String type, String height) {
-		this.type = type;
-		this.height = height;
-	}
-	
-	public String getType() {
-		return type;
+	public Point getPoint1() {
+		return point1;
 	}
 
-	public void setType(String type) {
-		this.type = type;
+	public void setPoint1(Point point1) {
+		this.point1 = point1;
+	}
+
+	public Point getPoint2() {
+		return point2;
+	}
+
+	public void setPoint2(Point point2) {
+		this.point2 = point2;
+	}
+
+	public Point getPoint3() {
+		return point3;
+	}
+
+	public void setPoint3(Point point3) {
+		this.point3 = point3;
+	}
+
+	public void draw() {
+		System.out.println("Point1 ( "+getPoint1().getX()+","+getPoint1().getY()+")");
+		System.out.println("Point1 ( "+getPoint2().getX()+","+getPoint2().getY()+")");
+		System.out.println("Point1 ( "+getPoint3().getX()+","+getPoint3().getY()+")");
+	}
+
+	public void destroy() throws Exception {
+		System.out.println("DisposableBean destroy method is called");
+	}
+
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("InitializingBean init method is called");
 	}
 	
-	public void draw() {
-		System.out.println(type+" Triangle shape drawn of height "+height);
+	public void myInIt() {
+		System.out.println("my init method is called");
+	}
+	
+	public void cleanUp() {
+		System.out.println("cleanUp method is called");
 	}
 
 }
