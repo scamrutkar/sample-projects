6e8a8968059716202d5ca6b80e9fdec6a6a25ac6
sagar.amrutkar <scamrutkar@gmail.com>
2020-09-10 19:39:25 +0530

    Adding multi-module-app strcture

---
+++ multi-module-app/backend/src/test/java/com/javacase/sagar/app/MultiModuleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacase.sagar.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
