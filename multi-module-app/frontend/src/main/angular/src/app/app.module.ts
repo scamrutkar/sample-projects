cff34e2219f188478f74aa60b9595af74f2f5f0f
sagar.amrutkar <scamrutkar@gmail.com>
2020-09-15 17:19:03 +0530

    Added angular material in frontend service

--- multi-module-app/frontend/src/main/angular/src/app/app.module.ts
+++ multi-module-app/frontend/src/main/angular/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
