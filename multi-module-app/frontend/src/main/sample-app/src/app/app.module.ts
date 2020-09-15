ad5819a2b565e28d2f839c26c0e418b283747124
sagar.amrutkar <scamrutkar@gmail.com>
2020-09-15 18:42:44 +0530

    changes frontend service strcture

--- multi-module-app/frontend/src/main/sample-app/src/app/app.module.ts
+++ multi-module-app/frontend/src/main/sample-app/src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
-import { MatSliderModule } from '@angular/material/slider';
-import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSliderModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
