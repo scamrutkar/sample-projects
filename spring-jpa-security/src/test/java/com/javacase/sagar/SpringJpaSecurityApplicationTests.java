44a0925b0adc4ca26cf1a0e6e3574cab99a99bc7
Sagar Amrutkar <scamrutkar@gmail.com>
2021-09-14 20:04:14 +0530

    Adding spring jpa security project

---
+++ spring-jpa-security/src/test/java/com/javacase/sagar/SpringJpaSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacase.sagar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
